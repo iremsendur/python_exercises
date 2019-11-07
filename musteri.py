af0c9bb9e0e44e63d229f78e4974b660ee61f8b7
irem <iremsendur1@gmail.com>
2019-11-07 03:32:42 +0300

    python variables example

---
+++ musteri.py
@@ -0,0 +1,9 @@
+#Variables for customer info
+mAdi = "İrem"
+mSoyadi = "Sendur"
+mAdSoyad = mAdi + " " + mSoyadi
+mCinsiyet = "Kadin"
+mTc = "51376514545"
+mDyil = 1994
+mAdres = "Istanbul"
+mYas = 2019 - mYas
