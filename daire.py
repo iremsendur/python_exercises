9407ea2937a9ce600c25e7a9f33dc63b9f16fc32
irem <iremsendur1@gmail.com>
2019-11-07 03:40:09 +0300

    python variables example 2

---
+++ daire.py
@@ -0,0 +1,7 @@
+'''calculation of circle's diameter and circumference'''
+r = input("Dairenin yaricapini girin: ")
+print(type(r))
+pi = 3.14
+cevre = 2 * pi * r
+alan = pi * r**2
+print("Dairenin cevresi: " + str(cevre) + " Dairenin alani: " + str(alan))
